616c3907e2215363cbb15595f46a502b0bbbbb41
TomoyaNishi <nishi.tomoya.519@gmail.com>
2022-03-30 00:16:09 +0900

    相手の石を置くタイミングを1000ミリ秒後に変更

--- src/component/board.js
+++ src/component/board.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { Row } from "./row";
 import { CreateBoard } from "./createOthello";
 
@@ -6,13 +6,27 @@ export const othello = new CreateBoard();
 
 export const Board = () => {
   const [othelloBoard, setOthelloBoard] = useState(othello.board);
-  // const [player, setPlayer] = useState("x");
   const rowArr = [0, 1, 2, 3, 4, 5];
   const player = "x";
   const opponent = "o";
 
   // 石が置ける場所をチェックする
   const putPositionArr = othello.putPosition(player);
+
+  // 相手の石を自動的に置く関数
+  function putOpponent() {
+    setTimeout(function () {
+      const opponentPutPositionArr = othello.putPosition(opponent);
+      const selectPosition = Math.floor(
+        Math.random() * opponentPutPositionArr.length
+      );
+      const opponentPut = opponentPutPositionArr[selectPosition];
+
+      othello.putStone(opponentPut[0], opponentPut[1], opponent);
+      const newArray = [...othelloBoard];
+      setOthelloBoard(newArray);
+    }, 1000);
+  }
 
   const flatOthelloBoard = othelloBoard.flat(2);
   let playerLength = flatOthelloBoard.filter((n) => n === player).length;
@@ -28,13 +42,7 @@ export const Board = () => {
       return;
     }
 
-    // 相手の石を自動的に置く
-    const opponentPutPositionArr = othello.putPosition(opponent);
-    const selectPosition = Math.floor(
-      Math.random() * opponentPutPositionArr.length
-    );
-    const opponentPut = opponentPutPositionArr[selectPosition];
-    othello.putStone(opponentPut[0], opponentPut[1], opponent);
+    putOpponent();
   }
 
   return (
@@ -54,7 +62,7 @@ export const Board = () => {
         })}
       </div>
       <div className="othello-state">
-        <p>player : x</p>
+        <p>player : black</p>
         <p>player length : {playerLength}</p>
         <p>opponent length : {opponentLength}</p>
       </div>
